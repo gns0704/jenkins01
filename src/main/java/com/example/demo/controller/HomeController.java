9e82216ff7334081a4d36efce03dfe936dec708c
gns0704 <gynam@hanmail.net>
2025-10-26 16:23:27 +0900

    jjj

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/")
 	public String home() {
-		return "222222222222222 world!!!";
+		return "99999999999999world!!!";
 	}
 }
